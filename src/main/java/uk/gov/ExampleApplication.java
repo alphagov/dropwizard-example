901a4501972b864900799527be7681e16aa96492
fjavierm <javier.marquez@digital.cabinet-office.gov.uk>
2018-02-16 08:24:04 +0000

    Work with local versions using MetricName

--- src/main/java/uk/gov/ExampleApplication.java
+++ src/main/java/uk/gov/ExampleApplication.java
@@ -1,50 +1,47 @@
 package uk.gov;
 
-import com.codahale.metrics.MetricRegistry;
 import io.dropwizard.Application;
 import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.metrics5.MetricRegistry;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.dropwizard.DropwizardExports;
 import io.prometheus.client.exporter.MetricsServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import uk.gov.ida.dropwizard.logstash.LogstashBundle;
 import uk.gov.resources.HelloWorld;
 
 public class ExampleApplication extends Application<ExampleConfiguration> {
-    public static void main(String[] args) throws Exception {
-        if (args == null || args.length == 0) {
-            new ExampleApplication().run("server", System.getenv("CONFIG_FILE"));
-        } else {
-            new ExampleApplication().run(args);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		if (args == null || args.length == 0) {
+			new ExampleApplication().run("server", System.getenv("CONFIG_FILE"));
+		} else {
+			new ExampleApplication().run(args);
+		}
+	}
 
-    @Override
-    public String getName() {
-        return "dropwizard-example";
-    }
+	@Override
+	public String getName() {
+		return "dropwizard-example";
+	}
 
-    @Override
-    public void initialize(final Bootstrap<ExampleConfiguration> bootstrap) {
-        bootstrap.setConfigurationSourceProvider(
-                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
-                        new EnvironmentVariableSubstitutor(false)
-                )
-        );
-        bootstrap.addBundle(new LogstashBundle());
-        MetricRegistry metrics = bootstrap.getMetricRegistry();
-        CollectorRegistry.defaultRegistry.register(new DropwizardExports(metrics));
-    }
+	@Override
+	public void initialize(final Bootstrap<ExampleConfiguration> bootstrap) {
+		bootstrap.setConfigurationSourceProvider(
+				new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+						new EnvironmentVariableSubstitutor(false)
+				)
+		);
+//		bootstrap.addBundle(new LogstashBundle());
+		MetricRegistry metrics = bootstrap.getMetricRegistry();
+		CollectorRegistry.defaultRegistry.register(new DropwizardExports(metrics));
+	}
 
-    @Override
-    public void run(final ExampleConfiguration configuration,
-                    final Environment environment) {
-        environment.jersey().register(new HelloWorld());
-        environment.servlets().addServlet("prometheus", new MetricsServlet()).addMapping("/prometheus");
-    }
+	@Override
+	public void run(final ExampleConfiguration configuration,
+					final Environment environment) {
+		environment.jersey().register(new HelloWorld());
+		environment.servlets().addServlet("metrics", new MetricsServlet()).addMapping("/metrics");
+	}
 
 }
--- src/main/java/uk/gov/resources/HelloWorld.java
+++ src/main/java/uk/gov/resources/HelloWorld.java
@@ -1,5 +1,7 @@
 package uk.gov.resources;
 
+import io.prometheus.client.Counter;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,9 +9,17 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class HelloWorld {
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String helloWorld() {
-        return "Hello, world!";
-    }
+
+	static final Counter requests = Counter.build()
+			.name("requests_total")
+			.labelNames("a1", "a2")
+			.help("Total requests.").register();
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWorld() {
+		requests.inc();
+
+		return "Hello, world!";
+	}
 }
