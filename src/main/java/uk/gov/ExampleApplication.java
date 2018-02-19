bd5695c53145fb802e16870b5c7b3d64d035b428
fjavierm <javier.marquez@digital.cabinet-office.gov.uk>
2018-02-19 16:05:19 +0000

    New artifact name

--- src/main/java/uk/gov/ExampleApplication.java
+++ src/main/java/uk/gov/ExampleApplication.java
@@ -1,67 +1,43 @@
 package uk.gov;
 
-import com.codahale.metrics.MetricRegistry;
+import engineering.reliability.gds.metrics.bundle.MetricsBundle;
 import io.dropwizard.Application;
 import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.dropwizard.DropwizardExports;
-import io.prometheus.client.exporter.MetricsServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import uk.gov.ida.dropwizard.logstash.LogstashBundle;
-import uk.gov.reng.metrics.bundle.MetricsBundle;
-import uk.gov.reng.metrics.config.Configuration;
-import uk.gov.reng.metrics.filter.AuthenticationFilter;
 import uk.gov.resources.HelloWorld;
 
-import javax.servlet.DispatcherType;
-import java.util.EnumSet;
-
 public class ExampleApplication extends Application<ExampleConfiguration> {
-    public static void main(String[] args) throws Exception {
-        if (args == null || args.length == 0) {
-            new ExampleApplication().run("server", System.getenv("CONFIG_FILE"));
-        } else {
-            new ExampleApplication().run(args);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		if (args == null || args.length == 0) {
+			new ExampleApplication().run("server", System.getenv("CONFIG_FILE"));
+		} else {
+			new ExampleApplication().run(args);
+		}
+	}
 
-    @Override
-    public String getName() {
-        return "dropwizard-example";
-    }
+	@Override
+	public String getName() {
+		return "dropwizard-example";
+	}
 
-    @Override
-    public void initialize(final Bootstrap<ExampleConfiguration> bootstrap) {
-        bootstrap.setConfigurationSourceProvider(
-                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
-                        new EnvironmentVariableSubstitutor(false)
-                )
-        );
-        bootstrap.addBundle(new LogstashBundle());
-        bootstrap.addBundle(new MetricsBundle());
-//        MetricRegistry metrics = bootstrap.getMetricRegistry();
-//        CollectorRegistry.defaultRegistry.register(new DropwizardExports(metrics));
-    }
+	@Override
+	public void initialize(final Bootstrap<ExampleConfiguration> bootstrap) {
+		bootstrap.setConfigurationSourceProvider(
+				new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+						new EnvironmentVariableSubstitutor(false)
+				)
+		);
+		bootstrap.addBundle(new LogstashBundle());
+		bootstrap.addBundle(new MetricsBundle());
+	}
 
-    @Override
-    public void run(final ExampleConfiguration configuration,
-                    final Environment environment) {
-//        final Configuration conf = Configuration.getInstance();
-
-        environment.jersey().register(new HelloWorld());
-//        environment.servlets().addServlet("prometheus", new MetricsServlet())
-//                .addMapping(conf.getPrometheusMetricsPath());
-//
-//        environment.servlets()
-//                .addFilter("AuthenticationFilter", new AuthenticationFilter())
-//                .addMappingForUrlPatterns(
-//                        EnumSet.of(DispatcherType.REQUEST),
-//                        true,
-//                        conf.getPrometheusMetricsPath());
-    }
+	@Override
+	public void run(final ExampleConfiguration configuration,
+					final Environment environment) {
+		environment.jersey().register(new HelloWorld());
+	}
 
 }
